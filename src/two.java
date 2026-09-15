3b59fb836704ae9316159735bd9471ac4485dc07
youngju0zoo <youngju0zoo@gmail.com>
2026-09-15 10:03:07 +0900

    두 수의 곱 구하기

---
+++ src/two.java
@@ -0,0 +1,15 @@
+public class two {
+
+    //정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+    public static void main(String[] args) {
+
+    }
+
+    class Solution {
+        public int solution(int num1, int num2) {
+            int answer = (num1 * num2);
+            System.out.println("answer = " + answer);
+            return answer;
+        }
+    }
+}
