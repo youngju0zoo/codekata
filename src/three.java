10545da7847122c1076f31e52aab580e42d03961
youngju0zoo <youngju0zoo@gmail.com>
2026-09-16 09:22:37 +0900

    두 수의 몫 구하기

---
+++ src/three.java
@@ -0,0 +1,11 @@
+public class three {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public int solution(int num1, int num2) {
+            int answer = num1 / num2;
+            return answer;
+        }
+    }
+}
