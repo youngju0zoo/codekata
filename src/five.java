b919cff421ce61499c3b88274958856db7cc76ee
youngju0zoo <youngju0zoo@gmail.com>
2026-09-18 09:51:57 +0900

    9월 18일 코드카타 풀이

---
+++ src/five.java
@@ -0,0 +1,29 @@
+public class five {
+    public static void main(String[] args) {
+
+    }
+    // 숫자 비교하기
+    class Solution {
+        public int solution(int num1, int num2) {
+            if (num1 == num2) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+    }
+    // 두 수의 합
+    class Solution {
+        public int solution(int num1, int num2) {
+            int answer = (num1 + num2);
+            return answer;
+        }
+    }
+    // 두 수의 나눗셈
+    class Solution {
+        public int solution(int num1, int num2) {
+            int answer = (num1 * 1000) / num2;
+            return answer;
+        }
+    }
+}
